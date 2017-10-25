d5da5503d8fa2028fdf21c57bc2edef1c5a60d24
Siddarth KM <siddarth.km@samsung.com>
2017-10-25 18:09:54 +0900

    Updating combine.sh script
    Signed-off-by: Siddarth KM <siddarth.km@samsung.com>

--- iot-preview/tools/setup_wizard/combine.sh
+++ iot-preview/tools/setup_wizard/combine.sh
@@ -1,1 +1,2 @@
-cat *.part* > org.tizen.iotsetupwizard_0.4.1.preview.jar
+cat org.tizen.iotsetupwizard_0.4.1.preview.jar.part* > org.tizen.iotsetupwizard_0.4.1.preview.jar
+rm org.tizen.iotsetupwizard_0.4.1.preview.jar.part*
