651e0a412f0c1113a50e474b6d093c10a83dc909
Sunggyu Choi <sunggyu.choi@samsung.com>
2018-05-21 14:59:05 +0900

    Add Native 5.0 Mobile/Wearable API Reference

---
+++ docs/application/native/api/mobile/5.0/group__elm__accessibility__group.js
@@ -0,0 +1,7 @@
+var group__elm__accessibility__group =
+[
+    [ "Access", "group__Elm__Access.html", null ],
+    [ "Atspi Accessible", "group__Elm__Interface__Atspi__Accessible.html", null ],
+    [ "Atspi Bridge", "group__Elm__Atspi__Bridge.html", null ],
+    [ "Atspi Component", "group__Elm__Interface__Atspi__Component.html", null ]
+];
