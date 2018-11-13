cd7085cd5525fb1c49e93ba0db8c39a763c8820c
hai.vt <hai.vt@samsung.com>
2018-11-13 14:29:21 +0700

    add button gototop in api reference page

--- docs/application/native/api/wearable/3.0/navtree.js
+++ docs/application/native/api/wearable/3.0/navtree.js
@@ -19,6 +19,21 @@ $(document).ready(function () {
     
     var div_search = '<div class="flexbox"><div class="flex1"></div><div class="tizen-search-api" style="float: right;"><form id="form-search-api"><div class="form-group-search"><input type="text" placeholder="Search related API References" name="search-api" id="search-api" style="padding-right: 38px !important;"><input type="text" name="category-application" id="category-application" style="display: none" value="native-application"><button class="btn btn-submit" id="btn-search-api"><svg width="32" height="32" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1216 832q0-185-131.5-316.5t-316.5-131.5-316.5 131.5-131.5 316.5 131.5 316.5 316.5 131.5 316.5-131.5 131.5-316.5zm512 832q0 52-38 90t-90 38q-54 0-90-38l-343-342q-179 124-399 124-143 0-273.5-55.5t-225-150-150-225-55.5-273.5 55.5-273.5 150-225 225-150 273.5-55.5 273.5 55.5 225 150 150 225 55.5 273.5q0 220-124 399l343 343q37 37 37 90z"></path></svg></button></div></form></div></div>';
     $('#doc-content').prepend(div_search);
+	
+	var btGotoTop = '<div><input type="image" id="btngototop" src="/images/btn_go_to_top.png" style="display: none"></div>';
+    $('body').append(btGotoTop);
+    
+    var btnTop = $('#btngototop');
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+          btnTop.show();
+        } else {
+          btnTop.hide();
+        }
+    });
+    btnTop.click(function () {
+        $('html, body').animate({scrollTop:0}, '300');
+    });
 });
 
 function capitalizeFirstLetter(string) {
