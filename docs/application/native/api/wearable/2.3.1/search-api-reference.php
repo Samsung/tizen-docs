055b7908b4442ae76fae8626df0b7931e38a6f57
hai.vt <hai.vt@samsung.com>
2018-11-20 16:44:30 +0700

    fix error font in search api reference page

--- docs/application/native/api/wearable/2.3.1/search-api-reference.php
+++ docs/application/native/api/wearable/2.3.1/search-api-reference.php
@@ -103,6 +103,7 @@ if(strlen($search_text) < 4){
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("utf8");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
