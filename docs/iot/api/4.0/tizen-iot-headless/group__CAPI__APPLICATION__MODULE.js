41f539e51b4126480aecf18ae06790e76acc151a
Young-Ae Kang <youngae.kang@samsung.com>
2019-10-11 17:44:01 +0900

    Add IoT Headless API Reference of Tizen IoT 4.0

---
+++ docs/iot/api/4.0/tizen-iot-headless/group__CAPI__APPLICATION__MODULE.js
@@ -0,0 +1,8 @@
+var group__CAPI__APPLICATION__MODULE =
+[
+    [ "App Common", "group__CAPI__APP__COMMON__MODULE.html", null ],
+    [ "App Control", "group__CAPI__APP__CONTROL__MODULE.html", null ],
+    [ "Event", "group__CAPI__EVENT__MODULE.html", null ],
+    [ "Preference", "group__CAPI__PREFERENCE__MODULE.html", null ],
+    [ "Resource Manager", "group__CAPI__RESOURCE__MANAGER__MODULE.html", null ]
+];
