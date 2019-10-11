0e9c9b54655324dce0b89dac58d424e6c3f93b95
Young-Ae Kang <youngae.kang@samsung.com>
2019-10-11 17:39:30 +0900

    Add IoT Headed API Reference of Tizen IoT 4.0

---
+++ docs/iot/api/4.0/tizen-iot-headed/group__Ecore.js
@@ -0,0 +1,15 @@
+var group__Ecore =
+[
+    [ "Ecore Application functions", "group__Ecore__Application__Group.html", null ],
+    [ "Ecore Getopt", "group__Ecore__Getopt__Group.html", null ],
+    [ "Ecore Input", "group__Ecore__Input__Group.html", null ],
+    [ "Ecore initialization, shutdown functions and reset on fork.", "group__Ecore__Init__Group.html", null ],
+    [ "Ecore main loop", "group__Ecore__Main__Loop__Group.html", "group__Ecore__Main__Loop__Group" ],
+    [ "Ecore time functions", "group__Ecore__Time__Group.html", null ],
+    [ "Ecore_Buffer - Graphics buffer functions", "group__Ecore__Buffer__Group.html", "group__Ecore__Buffer__Group" ],
+    [ "Ecore_Con - Connection functions", "group__Ecore__Con__Group.html", "group__Ecore__Con__Group" ],
+    [ "Ecore_Evas wrapper/helper set of functions", "group__Ecore__Evas__Group.html", "group__Ecore__Evas__Group" ],
+    [ "Ecore_File - Files and directories convenience functions", "group__Ecore__File__Group.html", null ],
+    [ "Ecore_IMF - Ecore Input Method Library Functions", "group__Ecore__IMF__Lib__Group.html", "group__Ecore__IMF__Lib__Group" ],
+    [ "Ecore_IPC - Ecore inter-process communication functions.", "group__Ecore__IPC__Group.html", "group__Ecore__IPC__Group" ]
+];
