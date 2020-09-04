2f74ab0e855e8a4ce5198dcd2238bc3c08897ff7
Mijin, Cho <mijin85.cho@samsung.com>
2020-09-04 10:11:52 +0900

    Update Native 5.5 Wearable API files
    Signed-off-by: Mijin, Cho <mijin85.cho@samsung.com>

---
+++ docs/application/native/api/wearable/5.5/group__Eina__Core__Group.js
@@ -0,0 +1,6 @@
+var group__Eina__Core__Group =
+[
+    [ "Hamster", "group__Eina__Hamster__Group.html", null ],
+    [ "Main", "group__Eina__Main__Group.html", null ],
+    [ "Types", "group__Eina__Types__Group.html", null ]
+];
