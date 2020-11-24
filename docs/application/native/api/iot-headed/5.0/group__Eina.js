b4510fa879bfda81a09847d2d75bd570a24742d9
Sunggyu Choi <sunggyu.choi@samsung.com>
2020-11-24 11:13:41 +0900

    Update Iot Headed/Headless API ref - Target: 4.0 / 5.0 / 5.5 - Removed unnecessary modules - Fixed some minor issues (broken link, etc)

--- docs/application/native/api/iot-headed/5.0/group__Eina.js
+++ docs/application/native/api/iot-headed/5.0/group__Eina.js
@@ -2,7 +2,9 @@ var group__Eina =
 [
     [ "Data Types", "group__Eina__Data__Types__Group.html", "group__Eina__Data__Types__Group" ],
     [ "Event Log Debugging", "group__Eina__Evlog.html", null ],
+    [ "Free Queue Group", "group__Eina__FreeQ__Group.html", null ],
     [ "Mmap Group", "group__Eina__Mmap__Group.html", null ],
+    [ "Short lived strings", "group__Eina__Slstr.html", null ],
     [ "Thread Queue Group", "group__Eina__Thread__Queue__Group.html", null ],
     [ "Tools", "group__Eina__Tools__Group.html", "group__Eina__Tools__Group" ]
 ];
--- docs/application/native/api/iot-headless/4.0/group__CAPI__SECURITY__FRAMEWORK.js
+++ docs/application/native/api/iot-headless/4.0/group__CAPI__SECURITY__FRAMEWORK.js
@@ -1,6 +1,5 @@
 var group__CAPI__SECURITY__FRAMEWORK =
 [
-    [ "Device Policy Manager", "group__CAPI__SECURITY__DPM__MODULE.html", "group__CAPI__SECURITY__DPM__MODULE" ],
     [ "Key Manager", "group__CAPI__KEY__MANAGER__MODULE.html", "group__CAPI__KEY__MANAGER__MODULE" ],
     [ "OpenSSL", "group__OPENSRC__OPENSSL__FRAMEWORK.html", null ]
 ];
