1ab45920cb245c1343e7b85fd17e1b2adc782521
Sunggyu Choi <sunggyu.choi@samsung.com>
2020-12-14 17:29:45 +0900

    Update Mobile/Wearabe/Headed/Headless 5.0 / 5.5 / 6.0 Native API ref

--- docs/application/native/api/iot-headed/6.0/group__dali.js
+++ docs/application/native/api/iot-headed/6.0/group__dali.js
@@ -2,5 +2,5 @@ var group__dali =
 [
     [ "DALi Adaptor (Deprecated)", "group__dali__adaptor.html", "group__dali__adaptor" ],
     [ "DALi Core (Deprecated)", "group__dali__core.html", "group__dali__core" ],
-    [ "DALi Toolkit", "group__dali__toolkit.html", "group__dali__toolkit" ]
+    [ "DALi Toolkit (Deprecated)", "group__dali__toolkit.html", "group__dali__toolkit" ]
 ];
