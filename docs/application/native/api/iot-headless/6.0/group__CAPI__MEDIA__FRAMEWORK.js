3835ae851dda8378ceccb0d703f0db7435460161
Sunggyu Choi <sunggyu.choi@samsung.com>
2021-05-07 16:34:22 +0900

    Update Native 6.0 Iot Headless API Ref for Audio IO and Wav Player (#1433)

--- docs/application/native/api/iot-headless/6.0/group__CAPI__MEDIA__FRAMEWORK.js
+++ docs/application/native/api/iot-headless/6.0/group__CAPI__MEDIA__FRAMEWORK.js
@@ -1,6 +1,8 @@
 var group__CAPI__MEDIA__FRAMEWORK =
 [
+    [ "Audio I/O", "group__CAPI__MEDIA__AUDIO__IO__MODULE.html", "group__CAPI__MEDIA__AUDIO__IO__MODULE" ],
     [ "Media Tool", "group__CAPI__MEDIA__TOOL__MODULE.html", "group__CAPI__MEDIA__TOOL__MODULE" ],
     [ "Player", "group__CAPI__MEDIA__PLAYER__MODULE.html", "group__CAPI__MEDIA__PLAYER__MODULE" ],
-    [ "Sound Manager", "group__CAPI__MEDIA__SOUND__MANAGER__MODULE.html", "group__CAPI__MEDIA__SOUND__MANAGER__MODULE" ]
+    [ "Sound Manager", "group__CAPI__MEDIA__SOUND__MANAGER__MODULE.html", "group__CAPI__MEDIA__SOUND__MANAGER__MODULE" ],
+    [ "WAV Player", "group__CAPI__MEDIA__WAV__PLAYER__MODULE.html", null ]
 ];
