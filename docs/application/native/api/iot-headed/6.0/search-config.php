447dc4fd3ec318dfc771d912e4a0af4a9d60335b
Sunggyu Choi <sunggyu.choi@samsung.com>
2023-10-13 17:20:57 +0900

    Update 6.0/6.5/7.0 Native API Reference for new enum value of Key-Manager

--- docs/application/native/api/iot-headed/6.0/search-config.php
+++ docs/application/native/api/iot-headed/6.0/search-config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "<div id=\"side-nav\" class=\"ui-resizable side-nav-resizable\">\n  <div id=\"nav-tree\">\n    <div id=\"nav-tree-contents\">\n    </div>\n  </div>\n  <div id=\"splitbar\" style=\"-moz-user-select:none;\" \n       class=\"ui-resizable-handle\">\n  </div>\n</div>\n<script type=\"text/javascript\">\n  initNavTree('.html','');\n</script>\n<div id=\"doc-content\">\n",
-  'logo' => "Generated on Mon Aug 7 2023 16:22:10 for Tizen Native API by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.7.6.1 ",
+  'logo' => "Generated on Fri Oct 13 2023 10:46:26 for Tizen Native API by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.7.6.1 ",
 );
 
 </script>
