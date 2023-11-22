4d255b196b31c3c774755c38e8bec23541b10f7f
Sunggyu Choi <sunggyu.choi@samsung.com>
2023-11-22 15:46:29 +0900

    Add 8.0 Native API Reference

---
+++ docs/application/native/api/common/8.0/group__CAPI__MESSAGING__FRAMEWORK.js
@@ -0,0 +1,6 @@
+var group__CAPI__MESSAGING__FRAMEWORK =
+[
+    [ "Email (Deprecated)", "group__CAPI__MESSAGING__EMAIL__MODULE.html", null ],
+    [ "Messages (Deprecated)", "group__CAPI__MESSAGING__MESSAGES__MODULE.html", "group__CAPI__MESSAGING__MESSAGES__MODULE" ],
+    [ "Push", "group__CAPI__MESSAGING__PUSH__PUBLIC__MODULE.html", null ]
+];
