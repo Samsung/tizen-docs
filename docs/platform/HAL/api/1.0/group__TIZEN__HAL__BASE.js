f8a90782e3333175681ef136d04b0f67d8d66cf3
Sunggyu Choi <sunggyu.choi@samsung.com>
2025-07-16 07:26:44 +0900

    Add HAL base

---
+++ docs/platform/HAL/api/1.0/group__TIZEN__HAL__BASE.js
@@ -0,0 +1,16 @@
+var group__TIZEN__HAL__BASE =
+[
+    [ "Common Error", "group__TIZEN__PACKAGE__COMMON__ERROR.html", null ],
+    [ "Peripheral Input & Output", "group__TIZEN__PACKAGE__PERIPHERAL__IO.html", null ],
+    [ "Platform Config", "group__TIZEN__PACKAGE__LIBTZPLATOFORM__CONFIG.html", null ],
+    [ "Runtime Information", "group__TIZEN__PACKAGE__RUNTIME__INFO.html", null ],
+    [ "System Information", "group__TIZEN__PACKAGE__SYSTEM__INFO.html", null ],
+    [ "alsa-lib", "group__TIZEN__HAL__BASE__ALSA.html", null ],
+    [ "coregl", "group__TIZEN__PACKAGE__COREGL.html", null ],
+    [ "libdlog", "group__TIZEN__PACKAGE__LIBDLOG.html", null ],
+    [ "libdrm", "group__TIZEN__HAL__BASE__LIBDRM.html", null ],
+    [ "libsystemd", "group__TIZEN__PACKAGE__LIBSYSTEMD.html", null ],
+    [ "libtbm", "group__TIZEN__PACKAGE__LIBTBM.html", null ],
+    [ "libtpl-egl", "group__TIZEN__PACKAGE__LIBTPL__EGL.html", null ],
+    [ "libusb", "group__TIZEN__HAL__BASE__LIBUSB.html", null ]
+];
