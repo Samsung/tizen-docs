b9dbd0beee58f1529ccb65c490352daa028d5a10
Sunggyu Choi <sunggyu.choi@samsung.com>
2025-10-28 16:51:24 +0900

    Update HAL 1.0 API Reference

--- docs/platform/HAL/api/1.0/group__HALAPI__HAL__CONNECTIVITY.js
+++ docs/platform/HAL/api/1.0/group__HALAPI__HAL__CONNECTIVITY.js
@@ -1,4 +1,6 @@
 var group__HALAPI__HAL__CONNECTIVITY =
 [
-    [ "Bluetooth", "group__HALAPI__HAL__BLUETOOTH__MODULE.html", null ]
+    [ "Bluetooth", "group__HALAPI__HAL__BLUETOOTH__MODULE.html", null ],
+    [ "Nfc", "group__HALAPI__HAL__NFC__MODULE.html", null ],
+    [ "Wi-Fi", "group__HALAPI__HAL__WIFI__MODULE.html", null ]
 ];
